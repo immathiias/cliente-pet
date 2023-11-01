99a5da6594fe5ba581e29b3ac35af072b7c25623
Manoel Mathias <dev.mathiasx@gmail.com>
2023-11-01 14:45:37 -0300

    implementa fluxo de alteracao camada service

--- src/main/java/br/com/petz/clientepet/cliente/application/service/ClienteApplicationService.java
+++ src/main/java/br/com/petz/clientepet/cliente/application/service/ClienteApplicationService.java
@@ -5,12 +5,14 @@ import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 
+import br.com.petz.clientepet.cliente.application.api.ClienteAlteracaoRequest;
 import br.com.petz.clientepet.cliente.application.api.ClienteDetalhadoResponse;
 import br.com.petz.clientepet.cliente.application.api.ClienteListResponse;
 import br.com.petz.clientepet.cliente.application.api.ClienteRequest;
 import br.com.petz.clientepet.cliente.application.api.ClienteResponse;
 import br.com.petz.clientepet.cliente.application.repository.ClienteRepository;
 import br.com.petz.clientepet.cliente.domain.Cliente;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
@@ -54,6 +56,12 @@ public class ClienteApplicationService implements ClienteService {
 		clienteRepository.deleteCliente(cliente);
 		log.info("[finaliza] ClienteApplicationService - deleteClientePorId");
 		
+	}
+
+	@Override
+	public void patchAlteraCliente(UUID idCliente, @Valid ClienteAlteracaoRequest clienteAlteracaoRequest) {
+		log.info("[inicia] ClienteApplicationService - patchAlteraCliente");
+		log.info("[finaliza] ClienteApplicationService - patchAlteraCliente");
 	}
 
 }
--- src/main/java/br/com/petz/clientepet/cliente/application/service/ClienteService.java
+++ src/main/java/br/com/petz/clientepet/cliente/application/service/ClienteService.java
@@ -3,10 +3,12 @@ package br.com.petz.clientepet.cliente.application.service;
 import java.util.List;
 import java.util.UUID;
 
+import br.com.petz.clientepet.cliente.application.api.ClienteAlteracaoRequest;
 import br.com.petz.clientepet.cliente.application.api.ClienteDetalhadoResponse;
 import br.com.petz.clientepet.cliente.application.api.ClienteListResponse;
 import br.com.petz.clientepet.cliente.application.api.ClienteRequest;
 import br.com.petz.clientepet.cliente.application.api.ClienteResponse;
+import jakarta.validation.Valid;
 
 public interface ClienteService {
 	ClienteResponse criaCliente(ClienteRequest clienteRequest);
@@ -16,4 +18,6 @@ public interface ClienteService {
 	ClienteDetalhadoResponse buscaClientePorId(UUID idCliente);
 	
 	void deleteClientePorId(UUID idCliente);
+
+	void patchAlteraCliente(UUID idCliente, @Valid ClienteAlteracaoRequest clienteAlteracaoRequest);
 }
