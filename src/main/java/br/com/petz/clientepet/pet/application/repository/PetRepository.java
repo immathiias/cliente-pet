133bab46980ba04f16c94a860c24e548a6b3af4c
Manoel Mathias <dev.mathiasx@gmail.com>
2023-11-02 15:43:45 -0300

    implemeta fluxo do repository

--- src/main/java/br/com/petz/clientepet/pet/application/repository/PetRepository.java
+++ src/main/java/br/com/petz/clientepet/pet/application/repository/PetRepository.java
@@ -1,9 +1,14 @@
 package br.com.petz.clientepet.pet.application.repository;
 
+import java.util.List;
+import java.util.UUID;
+
 import br.com.petz.clientepet.pet.domain.Pet;
 
 public interface PetRepository {
 
 	Pet salvaPet(Pet pet);
+
+	List<Pet> buscaPetsDoClienteComID(UUID idCliente);
 
 }
--- src/main/java/br/com/petz/clientepet/pet/application/service/PetApplicationService.java
+++ src/main/java/br/com/petz/clientepet/pet/application/service/PetApplicationService.java
@@ -36,8 +36,9 @@ public class PetApplicationService implements PetService {
 	public List<PetClienteListResponse> buscaPetsDoClienteComID(UUID idCliente) {
 		log.info("[inicia] PetApplicationService - buscaPetsDoClienteComID");
 		clienteService.buscaClientePorId(idCliente);
+		List<Pet> petsDoCliente = petRepository.buscaPetsDoClienteComID(idCliente);
 		log.info("[finaliza] PetApplicationService - buscaPetsDoClienteComID");
-		return null;
+		return PetClienteListResponse.converte(petsDoCliente);
 	}
 
 }
